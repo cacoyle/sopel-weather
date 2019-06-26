78d4d366649a49bf429ba251adb6abc8079de631
Chris Coyle <ccoyle@gmail.com>
2019-06-25 21:59:02 -0400

    Convert to f-strings (Fixes #2)

--- wz.py
+++ wz.py
@@ -31,8 +31,6 @@ def get_temp_by_zip(zipcode):
 
     import requests
 
-    weather_fmt = "{0}, {1} Conditions: {2} | Temp: {3}F | High: {4}F, Low: {5}F | Humidity: {6}% | Sunrise: {7}, Sunset: {8} | Today's Forecast: {9}"
-
     location = requests.get(
         here_url,
         params={
@@ -43,14 +41,13 @@ def get_temp_by_zip(zipcode):
     )
 
     location_match = None
-    # Find local result
     for result in location.json()["Response"]["View"][-1]["Result"]:
         if result["Location"]["Address"]["Country"] in ["USA", "CAN"]:
             location_match = result
             break
 
-    if not location_match:
-        return("Unable to find a proper match for %s" % zipcode)
+    else:
+        return(f"Unable to find a proper match for {zipcode}")
 
     city = location_match["Location"]["Address"]["City"]
     state = location_match["Location"]["Address"]["State"]
@@ -58,34 +55,27 @@ def get_temp_by_zip(zipcode):
     gps_loc = location_match["Location"]["NavigationPosition"][-1]
 
     darksky_data = requests.get(
-        "%s/%s/%s,%s" % (
-            darksky_url,
-            darksky_key,
-            gps_loc["Latitude"],
-            gps_loc["Longitude"]
-        )
+        f"{darksky_url}/{darksky_key}/{gps_loc['Latitude']},{gps_loc['Longitude']}"
     )
 
     current = darksky_data.json()["currently"]
     forecast = darksky_data.json()["daily"]["data"]
 
-    return(weather_fmt.format(
-        city,
-        state,
-        current["summary"],
-        current["temperature"],
-        forecast[0]["temperatureHigh"],
-        forecast[0]["temperatureLow"],
-        "%.2lf" % (current["humidity"] * 100),
-        unix_to_localtime(forecast[0]["sunriseTime"]),
-        unix_to_localtime(forecast[0]["sunsetTime"]),
-        forecast[0]["summary"]
-    ))
+    return(
+        f"{city}, {state} Conditions: {current['summary']} | "
+        f"Temp: {current['temperature']} | "
+        f"High: {forecast[0]['temperatureHigh']}, Low: {forecast[0]['temperatureLow']} | "
+        f"Humidity: {current['humidity']*100:.2f}% | "
+        f"Sunrise: {unix_to_localtime(forecast[0]['sunriseTime'])}, "
+        f"Sunset: {unix_to_localtime(forecast[0]['sunsetTime'])} | "
+        f"Today's Forecast: {forecast[0]['summary']}"
+    )
 
 
 @sopel.module.commands('wz')
 def weatherbot(bot, trigger):
     bot.say(get_temp_by_zip(trigger.group(2)))
+
 
 if __name__ == "__main__":
     print(get_temp_by_zip('K4R1E5'))
