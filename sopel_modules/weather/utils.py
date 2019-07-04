6d8977dfc10d0f6c9ff018245273a5b7b091eb2e
Chris Coyle <ccoyle@gmail.com>
2019-07-03 23:09:05 -0400

    Add rudimentary geoip lookup support

--- sopel_modules/weather/utils.py
+++ sopel_modules/weather/utils.py
@@ -3,6 +3,8 @@ import re
 us = re.compile(r"^\d{5}(-{0,1}\d{4})?$")
 ca = re.compile(r"([ABCEGHJKLMNPRSTVXY]\d)([ABCEGHJKLMNPRSTVWXYZ]\d){2}", re.IGNORECASE)
 
+geolite_citydb = '_geoip_geolite2/GeoLite2-City.mmdb'
+
 def unix_to_localtime(t, tz="US/Eastern", fmt="%H:%M:%S"):
     """
     Convert unix timestamp to local time.
@@ -30,3 +32,29 @@ def postal_code(string):
         return "CAN"
     else:
         return None
+
+def geoip_lookup(string):
+    """
+    Lookup the location of an IP address.
+    """
+
+    result = None
+
+    import geoip2.database
+    import sys
+
+    from os import path
+
+    reader = None
+
+    for p in sys.path:
+        mmdb_path = path.join(p, geolite_citydb)
+        if path.exists(mmdb_path):
+            reader = geoip2.database.Reader(mmdb_path)
+
+    if not reader:
+        raise Exception("Unable to locate geoip database")
+
+    result = reader.city(string)
+
+    return(result)
